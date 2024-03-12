713908a0970b142be5605b73f63ef76f2cf37ae8
Gregory Marshall <gmarshall@dawan.fr>
2024-03-12 10:28:34 +0100

    other mouse events

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45, 
+            x: 0, 
+            y: 0
         }
     }, 
     methods: {
@@ -17,6 +19,17 @@ const app = Vue.createApp({
         }, 
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        }, 
+        handleEvent(event, data) {
+            console.log(event, event.type);
+
+            if(data) {
+                console.log(data);
+            }
+        }, 
+        handleMousemove(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
