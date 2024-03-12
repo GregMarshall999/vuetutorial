99f5a351e02742b2021258553639876615983a33
Gregory Marshall <gmarshall@dawan.fr>
2024-03-12 11:51:58 +0100

    data attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://en-recherche-active.com',
             showBooks: true,
             books: [
-                { title: 'Name of the wind',        author: 'patrick rothfuss' },
-                { title: 'The way of the kings',    author: 'brandon sanderson' },
-                { title: 'The final empire',        author: 'brandon sanderson' }
+                { title: 'Name of the wind',        author: 'patrick rothfuss', img: 'assets/babushka.jpg' },
+                { title: 'The way of the kings',    author: 'brandon sanderson', img: 'assets/eggman.jpg' },
+                { title: 'The final empire',        author: 'brandon sanderson', img: 'assets/stonks.jpg' }
             ]
         }
     }, 
