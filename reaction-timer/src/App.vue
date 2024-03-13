aae9fded6a61754cd35b292d46335626d3fdd09d
Gregory Marshall <gmarshall@dawan.fr>
2024-03-13 11:19:00 +0100

    custom event with data

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <p v-if="showResults">Reaction time: {{ score }}ms</p>
 </template>
 
 <script>
@@ -12,7 +13,9 @@ export default {
   data() {
     return {
       isPlaying: false, 
-      delay: null
+      delay: null, 
+      score: null, 
+      showResults: false
     }
   },
   components: {
@@ -22,6 +25,12 @@ export default {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    }, 
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     }
   }
 }
