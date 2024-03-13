d42016eedcb9dad9a54e8f7c121f0748b7c76c9b
Gregory Marshall <gmarshall@dawan.fr>
2024-03-13 17:12:11 +0100

    routerlinks

--- project-jobs/src/views/HomeView.vue
+++ project-jobs/src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, fugiat!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolorem placeat autem totam id ut blanditiis in, at nisi libero vitae expedita labore excepturi ipsam! Consectetur reiciendis ad dolore quos cumque cupiditate molestiae cum ut delectus earum quaerat, impedit vero rem magnam quisquam voluptatibus dolores ipsam voluptate error porro ipsa odit. Velit, harum dolor, natus reiciendis expedita iure maxime perferendis optio quia deleniti id a obcaecati, perspiciatis nulla. Aliquid rem officiis est. Consequuntur eveniet nemo aperiam adipisci, quidem ut ex perferendis non reprehenderit libero est nihil vitae at autem alias accusamus optio blanditiis vel consequatur quae sunt esse facilis a.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
