3b343d4bc488760902bcc43c990f49b09a395a99
Gregory Marshall <gmarshall@dawan.fr>
2024-03-13 17:31:17 +0100

    folder structure

---
+++ project-jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return  {
+            jobs: [
+                { title: 'UX Designer', id: 1, details: 'Lorem' },
+                { title: 'Web Designer', id: 2, details: 'Lorem' },
+                { title: 'Vue Designer', id: 3, details: 'Lorem' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
