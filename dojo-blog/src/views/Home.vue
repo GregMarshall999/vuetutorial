ab708415333bfd271ebd3f345a44d0c19066601f
Isekario <iseka.rio2846@gmail.com>
2024-03-17 15:42:11 +0100

    composition api basics

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    Home
+
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Home", 
+    setup() {
+        let name = 'mario'
+        let age = 30
+
+        const handleClick = () => {
+            console.log('You clicked me')
+        }
+
+        return {
+            name: name,
+            age, 
+            handleClick
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
