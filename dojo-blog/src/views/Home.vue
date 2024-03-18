5bded9f351f8835377211105db3ac6ba5e42c426
Gregory Marshall <gmarshall@dawan.fr>
2024-03-18 13:34:47 +0100

    fetching data in setup

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts"/>
-    <button @click="showPosts = !showPosts">Toggle posts</button>
-    <button @click="posts.pop()">Delete post</button>
+    <div v-if="error">{{ error }}</div>
+
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -17,23 +20,30 @@ export default {
       PostList
     },
     setup() {
-        const posts = ref([
-            { 
-              title: 'Welcome to the blog', 
-              body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
-              id: 1 
-            },
-            { 
-              title: 'Top 5 CSS tips', 
-              body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam volutpat commodo sed egestas egestas. Gravida quis blandit turpis cursus in. Urna et pharetra pharetra massa massa ultricies mi quis. Eget dolor morbi non arcu risus. At imperdiet dui accumsan sit amet. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Magna ac placerat vestibulum lectus mauris ultrices. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Bibendum est ultricies integer quis. Pellentesque habitant morbi tristique senectus et netus et. Sodales neque sodales ut etiam sit amet nisl purus. Vel facilisis volutpat est velit egestas dui.', 
-              id: 1 
-            }
-        ])
+        const posts = ref([])
+        const error = ref(null)
 
-        const showPosts = ref(true)
+        const load = async () => {
+          try {
+            let data = await fetch('http://localhost:3000/posts')
+            
+            if(!data.ok) {
+              throw Error('No data available!')
+            }
+
+            posts.value = await data.json()
+          }
+          catch (err) {
+            error.value = err.message
+            //console.log(error.value)
+          }
+        }
+
+        load()
 
         return {
-            posts, showPosts
+            posts, 
+            error
         }
     }
 }
